ca23eda78beab435bc1c4acfaae68f8429375106
Alina-bai <alina.baishukurova@gmail.com>
2025-09-17 06:44:08 +0400

    Week 1: Starting with Spring Boot

---
+++ src/main/java/newusefy/com/internship/HelloWorld.java
@@ -0,0 +1,13 @@
+package newusefy.com.internship;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+    @RestController
+    public class HelloWorld {
+
+        @GetMapping("/hello")
+        public String hello() {
+            return "Hello World";
+        }
+    }
