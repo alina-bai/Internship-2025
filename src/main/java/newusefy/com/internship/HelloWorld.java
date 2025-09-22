fd41199ada3a48f1df32c02ff78beb4eeb65704c
Alina-bai <alina.baishukurova@gmail.com>
2025-09-22 22:03:43 +0400

    Week 1 HelloWorld printing

--- src/main/java/newusefy/com/internship/HelloWorld.java
+++ src/main/java/newusefy/com/internship/HelloWorld.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
     @RestController
     public class HelloWorld {
 
-        @GetMapping("/hello")
+        @GetMapping("/hello") // --- Это роут то есть /hello откроет текст "Hello world"
         public String hello() {
             return "Hello World";
         }
