0cf3b7488583e71d6a6e607da3833ae4151a481f
Alina-bai <alina.baishukurova@gmail.com>
2025-10-02 20:35:23 +0400

    Week 2 UserRepository

---
+++ src/main/java/newusefy/com/internship/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package newusefy.com.internship.repository;
+
+import newusefy.com.internship.model.User; // импортируем нашу Entity
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    // базовые CRUD-методы создаются автоматически
+    // тут пока пусто – базовые методы создаются автоматически
+    /*this line I added on week3: Spring Data will implement this method for you based on the method name.
+     It returns null if no user is found (we’ll check for that).*/
+    User findByUsername(String username);
+}
