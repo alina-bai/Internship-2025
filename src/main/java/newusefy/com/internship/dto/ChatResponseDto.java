a9011521767acc85665ec76ef6142ecf53f98dde
Alina-bai <alina.baishukurova@gmail.com>
2025-11-06 23:03:28 +0400

    Week 6  Add ChatResponseDto: model for AI response output

---
+++ src/main/java/newusefy/com/internship/dto/ChatResponseDto.java
@@ -0,0 +1,10 @@
+package newusefy.com.internship.dto;
+
+public class ChatResponseDto {
+    private String response;
+
+    public ChatResponseDto() {}
+    public ChatResponseDto(String response) { this.response = response; }
+    public String getResponse() { return response; }
+    public void setResponse(String response) { this.response = response; }
+}
