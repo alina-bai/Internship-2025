d8d0d3622671fd5033c6975abaf6fb2c0ccb50a4
Alina-bai <alina.baishukurova@gmail.com>
2025-11-07 22:40:41 +0400

    Week 6  Add JwtUtil: JWT generation and validation logic

---
+++ src/main/java/newusefy/com/internship/security/JwtUtil.java
@@ -0,0 +1,48 @@
+package newusefy.com.internship.security;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private final Key key;
+    private final long expirationMs;
+
+    public JwtUtil(
+            @Value("${app.jwt.secret}") String secret,
+            @Value("${app.jwt.expirationMs}") long expirationMs
+    ) {
+        // генерируем ключ из строки
+        this.key = Keys.hmacShaKeyFor(secret.getBytes());
+        this.expirationMs = expirationMs;
+    }
+
+    public String generateToken(String username) {
+        Date now = new Date();
+        Date exp = new Date(now.getTime() + expirationMs);
+
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(now)
+                .setExpiration(exp)
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String validateAndGetUsername(String token) {
+        // бросит исключение, если токен невалиден/истёк/подпись неверна
+        Jws<Claims> claims = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token);
+
+        return claims.getBody().getSubject();
+    }
+}
+
