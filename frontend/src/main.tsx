38c52796a20bd58146d4ff8c2210e1a956262540
Alina-bai <alina.baishukurova@gmail.com>
2025-11-20 19:37:04 +0400

    Week 6-7  ⚙️ Updated main.tsx — routing setup & ProtectedRoute integration

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,11 @@
+// src/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import AppRouter from "./App"; // Импортируем наш новый AppRouter
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <AppRouter />
   </React.StrictMode>
 );
