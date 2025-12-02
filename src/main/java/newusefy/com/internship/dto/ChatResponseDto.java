4fd610ccb692d29bf4b6380809b6f6d83f894432
Alina-bai <alina.baishukurova@gmail.com>
2025-12-02 20:53:29 +0400

    Week 8 dd ChatResponseDto (AI response + session ID)

--- src/main/java/newusefy/com/internship/dto/ChatResponseDto.java
+++ src/main/java/newusefy/com/internship/dto/ChatResponseDto.java
@@ -1,10 +1,38 @@
 package newusefy.com.internship.dto;
 
+/**
+ * Ответ бэкенда на запрос /api/chat
+ * - response      -> текст ответа ИИ
+ * - chatSessionId -> ID сессии (чтобы фронт мог продолжать тот же чат)
+ */
 public class ChatResponseDto {
-    private String response;
 
-    public ChatResponseDto() {}
-    public ChatResponseDto(String response) { this.response = response; }
-    public String getResponse() { return response; }
-    public void setResponse(String response) { this.response = response; }
+    private String response;
+    private Long chatSessionId;
+
+    public ChatResponseDto() {
+    }
+
+    public ChatResponseDto(String response, Long chatSessionId) {
+        this.response = response;
+        this.chatSessionId = chatSessionId;
+    }
+
+    // --- геттеры/сеттеры ---
+
+    public String getResponse() {
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+
+    public Long getChatSessionId() {
+        return chatSessionId;
+    }
+
+    public void setChatSessionId(Long chatSessionId) {
+        this.chatSessionId = chatSessionId;
+    }
 }
