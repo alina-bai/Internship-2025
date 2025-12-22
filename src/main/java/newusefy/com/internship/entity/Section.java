bbab70224e312e98a21a1b293325cea3d2b13d1e
Alina-bai <alina.baishukurova@gmail.com>
2025-12-22 22:49:46 +0400

    Week 10 Section.java

---
+++ src/main/java/newusefy/com/internship/entity/Section.java
@@ -0,0 +1,24 @@
+package newusefy.com.internship.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Section {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id") // foreign key
+    private Course course;
+}
