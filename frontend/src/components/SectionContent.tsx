f8bed9404d6ed6f96f18de0e6ea0dd04fcd7e137
Alina-bai <alina.baishukurova@gmail.com>
2026-02-25 19:27:09 +0400

    week 14

--- frontend/src/components/SectionContent.tsx
+++ frontend/src/components/SectionContent.tsx
@@ -1,5 +1,23 @@
+// import React from "react"; // не нужно в React 17+
 import ReactMarkdown from "react-markdown";
 
-<ReactMarkdown>
-  {section.content}
-</ReactMarkdown>
+type Section = {
+    id: number;
+    title: string;
+    content: string;
+};
+
+type Props = {
+    section: Section | null;
+};
+
+export default function SectionContent({ section }: Props) {
+    if (!section) return <div>Выберите раздел</div>;
+
+    return (
+        <div>
+            <h2>{section.title}</h2>
+            <ReactMarkdown>{section.content}</ReactMarkdown>
+        </div>
+    );
+}
