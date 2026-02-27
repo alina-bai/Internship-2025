b66480301e5cbd7b09541359c7e8ee9bf7f776a3
Alina-bai <alina.baishukurova@gmail.com>
2026-02-27 15:45:46 +0400

    week 14

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -1,19 +1,11 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 
-interface ProtectedRouteProps {
-  children: JSX.Element;
-}
-
-const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
-  const token = localStorage.getItem("jwtToken");
+export default function ProtectedRoute() {
+  const token = localStorage.getItem("token");
 
   if (!token) {
-    alert("You must be logged in to access the chat");
-    return <Navigate to="/login" replace />;
+    return <Navigate to="/" />;
   }
 
-  return children;
-};
-
-export default ProtectedRoute;
+  return <Outlet />;
+}
